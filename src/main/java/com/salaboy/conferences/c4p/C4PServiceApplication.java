f9df272f06a667af6a301ae4ea266e5b47a0e195
salaboy <Salaboy@gmail.com>
2022-06-14 20:19:14 +0100

    adding instant for reachability

--- src/main/java/com/salaboy/conferences/c4p/C4PServiceApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/C4PServiceApplication.java
@@ -7,13 +7,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.TypeAccess;
-import org.springframework.nativex.hint.TypeHint;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-
-import java.sql.Driver;
+import java.time.Instant;
 
 @SpringBootApplication
 @EnableConfigurationProperties
