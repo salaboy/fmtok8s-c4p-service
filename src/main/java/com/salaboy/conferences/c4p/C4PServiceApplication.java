7c174981188b7e9d3f056a9bc2fd8cdb2c2e5687
salaboy <Salaboy@gmail.com>
2022-06-15 11:14:31 +0100

    adding native hints in main

--- src/main/java/com/salaboy/conferences/c4p/C4PServiceApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/C4PServiceApplication.java
@@ -1,5 +1,6 @@
 package com.salaboy.conferences.c4p;
 
+import io.r2dbc.postgresql.PostgresqlConnectionFactoryProvider;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +8,23 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import java.net.URI;
 import java.time.Instant;
+import java.time.ZonedDateTime;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-
+@NativeHint(
+        trigger = PostgresqlConnectionFactoryProvider.class,
+        types = {
+                @TypeHint(types = { Instant[].class, ZonedDateTime[].class, URI[].class }, access = {}),
+        }
+)
 public class C4PServiceApplication {
 
     public static void main(String[] args) {
